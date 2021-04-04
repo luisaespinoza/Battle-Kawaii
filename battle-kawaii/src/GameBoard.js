2c7adf2173f27e2c940cba6498c71cdfd3cd2a4c
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-04-04 11:11:40 -0700

    cleaned up code and added comments for reference. Next: check board has legal moves.

--- battle-kawaii/src/GameBoard.js
+++ battle-kawaii/src/GameBoard.js
@@ -1,5 +1,5 @@
 
-console.log("I live!")
+// console.log("I live!") 
 const puzzlePieces = ['Red','Green','Blue','Yellow']
 
 const createGameBoard = (x, y) => {
@@ -20,16 +20,14 @@ const getRandomPiece = () => {
 }
 
 const checkMatch = (x1, y1, x2, y2) => {
-
+  // is next piece on the board?
   let isXInBounds = ( x2 < board.length && 0 <= x2 )
   if(isXInBounds) {
     let isYInBounds = ( y2 < board[x2].length && 0 <= y2 )
     if(isYInBounds) {
-      console.log("(x1,y1) is = ",board[x1][y1])
-      console.log("(x2,y2) is = ",board[x2][y2])
+      // if so...
       let isNextMatch = (board[x1][y1] === board[x2][y2])
-      console.log(isXInBounds,isYInBounds,isNextMatch)
-      return isXInBounds && isYInBounds && isNextMatch
+      return isNextMatch
     }
   }
   return false
@@ -39,10 +37,12 @@ const checkMatch = (x1, y1, x2, y2) => {
 const checkNextIn=(x , y, axis, direction) => {
   // 'c' is our constant incrementer/decrementer for the recursive callback chain
   const c = (direction === '+') ? 1 : -1
+  // if the next tile on the axis matches: 
+  // add direction constant to axis coordinate and check next in line
+  // else return this value as the end of the series
   if (axis=== 'x') {
     if(checkMatch(x, y, x + c, y)) {
       let xNew = x + c
-      console.log("Launching recursive check.", xNew )
       // launch recursive callback for next in line
       return checkNextIn(xNew,y,axis,direction)
     } else {
@@ -52,7 +52,6 @@ const checkNextIn=(x , y, axis, direction) => {
   else {
     if (checkMatch(x, y, x, y + c)) {
       let yNew = y + c
-      console.log("Launching recursive check.", yNew)
       // launch recursive callback for next in line
       return checkNextIn(x,yNew,axis,direction)
     } else {
@@ -62,7 +61,7 @@ const checkNextIn=(x , y, axis, direction) => {
 }
 
 const checkMatches = (x, y) => {
-  let matchEndPoints = {
+  let matchResults = {
     hasMatches: false,
     start: [x,y],
     end:[x,y],
@@ -77,26 +76,29 @@ const checkMatches = (x, y) => {
 // '-' direction for start points |||| '+' direction for end points
   for(let j = 0; j <= 1 ; j++) {
     let axis = axes[j]
-    matchEndPoints.start[j] = checkNextIn(x,y,axis,directions[0])
-    matchEndPoints.end[j] = checkNextIn(x,y,axis,directions[1])
+    matchResults.start[j] = checkNextIn(x,y,axis,directions[0])
+    matchResults.end[j] = checkNextIn(x,y,axis,directions[1])
   } 
-  console.log(matchEndPoints.start)
-  console.log(matchEndPoints.end)
-
-  // check if there are any match series 3 or greater
-  let matchResults = matchEndPoints.end.map(function(coordinate,index){
-    return coordinate - matchEndPoints.start[index] + 1
+  // check length of series for each axis
+  // dump into new array [x,y]
+  let matchRanges = matchResults.end.map(function(coordinate,index){
+    return coordinate - matchResults.start[index] + 1
   })
-
-  console.log("Match results",matchResults[0],matchResults[1])
-  let xHasMatches = matchResults[0] >= 3
-  let yHasMatches = matchResults[1] >= 3
-  console.log("x has matches: ", xHasMatches, "y has matches: ", yHasMatches)
+  // check if there are any series of matches 3 or greater
+  let xHasMatches = matchRanges[0] >= 3
+  let yHasMatches = matchRanges[1] >= 3
   if (xHasMatches || yHasMatches){
-    matchEndPoints.hasMatches = true
+    matchResults.hasMatches = true
   }
-  console.log(matchEndPoints)
-  return matchEndPoints
+  // update match ranges anyway. A series of 2 can be used as
+  // candidates to check if the board has legal moves?? Hmm...
+  // Might need to create new propery called candidateForMatch
+  // and store any that flag true into an array, 
+  // instead of checking the entire board. 
+  matchResults.xMatchRange = xHasMatches
+  matchResults.yMatchRange = yHasMatches
+  // returns object of results 
+  return matchResults
 }
 
 const randomizeBoard = () => {
@@ -104,15 +106,17 @@ const randomizeBoard = () => {
   let y = board[0].length
   for(let i = 0 ; i < x ; i++){
     for( let j = 0; j < y ; j++){
+      // generate a random piece in place until a piece 
+      // that doesn't create a 3 series is generated.
       let isMatching = true
       while(isMatching) {
         board[i][j] = getRandomPiece()
         let matchesFound = checkMatches(i,j)
         isMatching = matchesFound.hasMatches
       }
+      // succeeding, carry on to next tile
     }
   }
-  console.log(board)
 }
 
 
